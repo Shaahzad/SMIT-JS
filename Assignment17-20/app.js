47d2fdb0733290bc663fc90c147e2269e622fc40
Muhammad shahzad <shehzadmemon147@gmail.com>
2023-10-09 19:37:24 +0500

    Assignment21-25

--- Assignment17-20/app.js
+++ Assignment17-20/app.js
@@ -1,31 +1,31 @@
 // Q1
 
-var arr = [
-    [],
-    [],
-    []
-]
+// var arr = []
+//     arr = [0],
+//     arr = [1],
+//     arr = [2],
+//     document.write(arr.length)
 
 
 // Q2
 
-var matrix = [
-    [0, 1, 2, 3, "<br>"]+
-    [1,0,1,2, "<br>"]+
-    [2,1,0,1]
-]
-document.write(matrix)
-document.write("<br>")
-document.write("<br>")
+// var matrix = [
+//     [0, 1, 2, 3, "<br>"]+
+//     [1,0,1,2, "<br>"]+
+//     [2,1,0,1]
+// ]
+// document.write(matrix)
+// document.write("<br>")
+// document.write("<br>")
 
 
 // Q3
 
-for (let i = 1; i <=10 ; i++) {
-document.write(i + "<br>")
-}
-document.write("<br>")
-document.write("<br>")
+// for (let i = 1; i <=10 ; i++) {
+// document.write(i + "<br>")
+// }
+// document.write("<br>")
+// document.write("<br>")
 
 
 
@@ -46,113 +46,113 @@ document.write("<br>")
 
 // Q5
 
-var fruit = ["apple","banana","mango","orange","strawberry"]
-document.write(fruit)
-document.write("<br>")
-document.write("<br>")
+// var fruit = ["apple","banana","mango","orange","strawberry"]
+// document.write(fruit)
+// document.write("<br>")
+// document.write("<br>")
 
 
-for (let i = 0; i < fruit.length; i++){
-    document.write("element at index " + i + " is " + fruit[i] + "<br>")
-}
-document.write("<br>")
-document.write("<br>")
+// for (let i = 0; i < fruit.length; i++){
+//     document.write("element at index " + i + " is " + fruit[i] + "<br>")
+// }
+// document.write("<br>")
+// document.write("<br>")
 
 // Q6 
 
 // a // counting
 
-for (let i = 1; i <=15 ; i++) {
-    document.write(i + " ")
-    }
-    document.write("<br>")
-    document.write("<br>")
+// for (let i = 1; i <=15 ; i++) {
+//     document.write(i + " ")
+//     }
+//     document.write("<br>")
+//     document.write("<br>")
 
     // b reverse counting
 
-    for (let i = 10; i >=1 ; i--) {
-        document.write(i + " ")
-        }
-    document.write("<br>")
-    document.write("<br>")
+    // for (let i = 10; i >=1 ; i--) {
+    //     document.write(i + " ")
+    //     }
+    // document.write("<br>")
+    // document.write("<br>")
 
     // c even number
 
-    for (let i = 0; i <=20 ; i++) {
-        if(i % 2 == 0){
-            document.write(i + " ")
-        }
-    }
-    document.write("<br>")
-    document.write("<br>")
+    // for (let i = 0; i <=20 ; i++) {
+    //     if(i % 2 == 0){
+    //         document.write(i + " ")
+    //     }
+    // }
+    // document.write("<br>")
+    // document.write("<br>")
 
     //  d odd number
 
-    for (let i = 0; i <=20 ; i++) {
-        if(i % 2 != 0){
-            document.write(i + " ")
-        }
-    }
-    document.write("<br>")
-    document.write("<br>")
+    // for (let i = 0; i <=20 ; i++) {
+    //     if(i % 2 != 0){
+    //         document.write(i + " ")
+    //     }
+    // }
+    // document.write("<br>")
+    // document.write("<br>")
 
     // e series
-    for (let i = 2; i <=20 ; i++) {
-        if(i % 2 == 0){
-            document.write(i + "K" +  " ")
-        }
-    }
-    document.write("<br>")
-    document.write("<br>")
+    // for (let i = 2; i <=20 ; i++) {
+    //     if(i % 2 == 0){
+    //         document.write(i + "K" +  " ")
+    //     }
+    // }
+    // document.write("<br>")
+    // document.write("<br>")
 
     // Q8
 
-    var Arr = [24,53,78,91,12]
-    document.write(Arr)
-    document.write("<br>")
-    document.write("<br>")
-    var max = Math.max(...Arr)
-    document.write("the largest number is " + max)
-    document.write("<br>")
-    document.write("<br>")
+    // var Arr = [24,53,78,91,12]
+    // document.write(Arr)
+    // document.write("<br>")
+    // document.write("<br>")
+    // var max = Math.max(...Arr)
+    // document.write("the largest number is " + max)
+    // document.write("<br>")
+    // document.write("<br>")
 
 // Q9
 
-var A = [24,53,78,91,12]
-document.write(A)
-document.write("<br>")
-document.write("<br>")
-var min = Math.min(...A)
-document.write("the smallest number is " + min)
-document.write("<br>")
-document.write("<br>")
+// var A = [24,53,78,91,12]
+// document.write(A)
+// document.write("<br>")
+// document.write("<br>")
+// var min = Math.min(...A)
+// document.write("the smallest number is " + min)
+// document.write("<br>")
+// document.write("<br>")
 
 // Q10
 
-for (let i = 5; i <= 100; i+=5){
-    document.write(i + " ")
-}
-document.write("<br>")
-document.write("<br>")
+// for (let i = 5; i <= 100; i+=5){
+//     document.write(i + " ")
+// }
+// document.write("<br>")
+// document.write("<br>")
 
 // Q7
   
-var User = prompt("enter which item you need")
-User = User.toLowerCase();
-var food = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var User = prompt("enter which item you need")
+// User = User.toLowerCase();
+// var food = ["cake", "apple pie", "cookie", "chips", "patties"]
 
-var match = false
+// var match = false
 
-var itemindex;
-for (var i = 0; i< food.length; i++){
-    var item = food[i].toLowerCase();
-    if (item === User){
-        match = true
-        itemindex = i;
-    }
-}
-if(match){
-    alert(User + " is available at counter number " + itemindex + " in our bakery")
-}else {
-    alert("we are sorry " + User + " is not available in our bakery")
-}
+// var itemindex;
+// for (var i = 0; i< food.length; i++){
+//     var item = food[i].toLowerCase();
+//     if (item === User){
+//         match = true
+//         itemindex = i;
+//     }
+// }
+// if(match){
+//     alert(User + " is available at counter number " + itemindex + " in our bakery")
+// }else {
+//     alert("we are sorry " + User + " is not available in our bakery")
+// }
---
+++ Assignment21-25/app.js
@@ -0,0 +1,119 @@
+// var firstName = prompt("Enter your first name")
+// var secondName = prompt("enter Your second name")
+
+// var fullName = firstName.concat(" " ,secondName)
+
+
+// document.write(fullName)
+
+
+
+// var userInp = prompt("enter your favourite mobile")
+
+
+// document.write(`my favourite phone is: ${userInp} length of string: ${userInp.length}`)
+
+
+// var countryName = "Pakistani"
+
+// var indexno = countryName.indexOf("n")
+
+// document.write(`string ${countryName} index of "n" : ${indexno}`)
+
+// var Name = "Hello World"
+
+// var lastindexNo = Name.lastIndexOf("l")
+
+//  document.write(`string ${Name} last index of "l" : ${lastindexNo}`)
+
+
+// var country = "Pakistani"
+
+//  var charfind = country.charAt(3)
+ 
+//  document.write(`string ${country} character @ index 3 : ${charfind}`)
+
+
+// var firstName = prompt("Enter your first name")
+// var secondName = prompt("enter Your second name")
+
+// var fullName = firstName.concat(" " ,secondName)
+
+
+// document.write(fullName)
+
+
+// var Name = "hyderabad"
+
+
+// var repl = Name.replace("hyder", "Islam")
+
+
+//  document.write(`city ${Name} After replacement : ${repl}`)
+
+
+// var message = "Ali and sami are best friend they play cricket and football together"
+
+
+// var replAll = message.replaceAll("and","&")
+
+
+//  document.write(` ${message} <br> ${replAll}`)
+
+
+
+// var str = "123"
+
+//   document.write(`${str} <br> ${typeof str} <br>`)
+
+
+//   var num = Number(str)
+
+//   document.write(` ${str} <br> ${typeof num}`)
+
+
+// var user = prompt("enter any thing").toUpperCase()
+
+//   document.write(`${user}`)
+
+// var user = prompt("enter any thing")
+// var firstlettercapital = user.charAt(0).toUpperCase() + user.slice(1)
+//   document.write(`${firstlettercapital}`)
+
+// var num = 123.8
+
+// document.write(`${num} <br>`)
+
+// var remove = parseInt(num)
+
+// document.write(`${num} <br>`)
+
+// var convert = remove.toString()
+
+
+
+
+// document.write(`${convert + 8}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
